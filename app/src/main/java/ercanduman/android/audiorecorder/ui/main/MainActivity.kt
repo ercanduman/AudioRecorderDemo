0c7902ebed4d4aa3c6a6b6c94d27985d06a1e605
ercan.duman <ercan.duman@check24.de>
2022-06-03 17:54:48 +0200

    Update design changes for Home screen layout fragment_home.xml

--- app/src/main/java/ercanduman/android/audiorecorder/ui/main/MainActivity.kt
+++ app/src/main/java/ercanduman/android/audiorecorder/ui/main/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import ercanduman.android.audiorecorder.R
 import ercanduman.android.audiorecorder.databinding.ActivityMainBinding
@@ -31,11 +30,6 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
