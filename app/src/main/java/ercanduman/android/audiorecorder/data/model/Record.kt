26e906c365f7c0ccadcee0671684332f692c812f
ercan.duman <ercan.duman@check24.de>
2022-06-05 20:32:59 +0200

    Update the format of the recording time

--- app/src/main/java/ercanduman/android/audiorecorder/data/model/Record.kt
+++ app/src/main/java/ercanduman/android/audiorecorder/data/model/Record.kt
@@ -1,10 +1,18 @@
 package ercanduman.android.audiorecorder.data.model
 
+import java.util.concurrent.TimeUnit
+
 data class Record(
     val name: String,
     val path: String,
     val startedTime: Long = System.currentTimeMillis(),
     val stoppedTime: Long = System.currentTimeMillis()
 ) {
-    val duration = (stoppedTime - startedTime) / 1000L
+    private val recodingTime = stoppedTime - startedTime
+    private val recodingTimeInMin = TimeUnit.MILLISECONDS.toMinutes(recodingTime)
+    val duration = String.format(
+        "%02d:%02d",
+        recodingTimeInMin,
+        TimeUnit.MILLISECONDS.toSeconds(recodingTime) - TimeUnit.MINUTES.toSeconds(recodingTimeInMin)
+    )
 }
